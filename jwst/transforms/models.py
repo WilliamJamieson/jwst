b25c17d59b6423f645b04c434bf81b3b205b9d00
Nadia Dencheva <nadia.dencheva@gmail.com>
2021-12-07 17:14:35 -0500

    Fs test passing

--- jwst/transforms/models.py
+++ jwst/transforms/models.py
@@ -696,6 +696,9 @@ class Gwa2Slit(Model):
     def __init__(self, slits, models):
         self.slit_names = [s.name for s in slits]
         self.slit_ids = [s.shutter_id for s in slits]
+        #self._slits = [tuple(s) for s in slits]
+        # get the shhutter_id
+        #self.slit_ids = [s[1] for s in slits]
         if isiterable(slits[0]):
             self._slits = [tuple(s) for s in slits]
             #self.slit_ids = [s[0] for s in self._slits]
@@ -733,8 +736,8 @@ class Gwa2Slit(Model):
             id = NRS_FS_N2ID[name]
         else:
             id = name
-        result = self.evaluate(id, x, y, z)
-        return result
+        return self.evaluate(id, x, y, z)
+        #return result
         #return (NRS_FS_ID2N[result[0]],) + result[1:]
 
 
@@ -772,11 +775,21 @@ class Slit2Msa(Model):
         # else:
         #     self._slits = list(slits)
         #     #self.slit_ids = self._slits
-        self._slits = list(slits)
-        print(self._slits)
-        self.slit_names = [s.name for s in self._slits]
-        self.slit_ids = [s.shutter_id for s in self._slits]
+        self.slit_names = [s.name for s in slits]
+        self.slit_ids = [s.shutter_id for s in slits]
+        # self._slits = list(slits)
+        # print(self._slits)
+        # self.slit_names = [s.name for s in self._slits]
+        # self.slit_ids = [s.shutter_id for s in self._slits]
         self.models = models
+        if isiterable(slits[0]):
+            self._slits = [tuple(s) for s in slits]
+            #self.slit_ids = [s[0] for s in self._slits]
+            print(slits)
+        else:
+            self._slits = list(slits)
+            #self.slit_ids = self._slits
+            print('else', slits)
 
     @property
     def slits(self):
@@ -800,8 +813,8 @@ class Slit2Msa(Model):
             id = NRS_FS_N2ID[name]
         else:
             id = name
-        result = self.evaluate(id, x, y)
-        return result
+        return self.evaluate(id, x, y)
+        #return result
         #return (NRS_FS_ID2N[result[0]],) + result[1:]
 
 
